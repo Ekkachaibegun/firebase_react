fcdbb07166fe245f691d43fb8b3457e952cb036e
Ekkachaibegun <120492275+Ekkachaibegun@users.noreply.github.com>
2023-06-25 06:46:53 +0700

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -12,9 +12,6 @@ const UserContext = createContext();
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
 
-  const createUser = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
-  };
 
    const signIn = (email, password) =>  {
     return signInWithEmailAndPassword(auth, email, password)
